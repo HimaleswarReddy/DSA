846e65f4e4dc19255147766c549e03c9eab95b62
Himaleswar Reddy <himaleswarreddy@gmal.com>
2024-03-18 12:31:57 +0530

    Finding sum

--- src/com/nani/Inputs.java
+++ src/com/nani/Inputs.java
@@ -5,8 +5,14 @@ import java.util.Scanner;
 public class Inputs {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        System.out.print("Enter your roll number:");
-        int rollNo = input.nextInt();
-        System.out.println("Your roll number is "+rollNo);
+        //    System.out.print("Enter your roll number:");
+        //  int rollNo = input.nextInt();
+        //System.out.println("Your roll number is "+rollNo);
+        int num1 = input.nextInt();
+        int num2 = input.nextInt();
+
+        int sum = num1 + num2;
+        System.out.println("Sum = "+sum);
+
     }
 }
