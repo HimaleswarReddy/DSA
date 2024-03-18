a94752d205a08ee31d430655fa54ba965b9dbbda
Himaleswar Reddy <himaleswarreddy@gmal.com>
2024-03-18 11:16:22 +0530

    java basics

---
+++ src/com/nani/Inputs.java
@@ -0,0 +1,12 @@
+package com.nani;
+
+import java.util.Scanner;
+
+public class Inputs {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter your roll number:");
+        int rollNo = input.nextInt();
+        System.out.println("Your roll number is "+rollNo);
+    }
+}
---
+++ src/com/nani/Primitives.java
@@ -0,0 +1,19 @@
+package com.nani;
+
+public class Primitives {
+    public static void main(String[] args) {
+        int rollNum = 9244;
+
+        char nameStartingLetter= 'H';
+
+        //String is not the primitive type
+        String name= "Himalesh";
+
+        float marks = 95.7f;
+        double largeDecimalNum = 4234.98574;
+        long largeIntegerNum = 242353466776578L;
+        boolean  check = false;
+
+
+    }
+}
