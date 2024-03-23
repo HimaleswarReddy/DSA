a710101f18cd86c6697c079dba50326dc2c47158
Himaleswar Reddy <120051052+HimaleswarReddy@users.noreply.github.com>
2024-03-23 13:05:09 +0530

    even or odd number by taking input using scanner class

---
+++ src/com/nani/EvenOrOdd.java
@@ -0,0 +1,15 @@
+package com.nani;
+
+import java.util.Scanner;
+
+public class EvenOrOdd {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num1 = sc.nextInt();
+        if (num1 % 2 == 0) {
+            System.out.println("Given number is even");
+        } else {
+            System.out.println("Given number is odd");
+        }
+    }
+}
