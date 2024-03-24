2d7cac067bd3a3936618077f1ed04822320578a4
Himaleswar Reddy <120051052+HimaleswarReddy@users.noreply.github.com>
2024-03-24 20:25:42 +0530

    finding reverse of number using while loop.

---
+++ src/com/nani/ReversingNum.java
@@ -0,0 +1,18 @@
+package com.nani;
+
+import java.util.Scanner;
+
+public class ReversingNum {
+    public static void main(String[] args) {
+        Scanner sc =  new Scanner(System.in);
+        System.out.print("Enter the  number for which you need to find reverse: ");
+        int num  =sc.nextInt();
+
+        System.out.print("Reverse of "+num+" is ");
+        while(num>0){
+            int rem = num%10;
+            System.out.print(rem);
+            num/=10;
+        }
+    }
+}
