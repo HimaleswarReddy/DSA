967348a128d10318051221ae6f255a183f1bc6c8
Himaleswar Reddy <120051052+HimaleswarReddy@users.noreply.github.com>
2024-03-24 20:12:50 +0530

    Finding the frequency of a Digit in number.

---
+++ src/com/nani/CountNums.java
@@ -0,0 +1,23 @@
+package com.nani;
+
+import java.util.Scanner;
+
+public class CountNums {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the number: ");
+        int num= sc.nextInt();
+        System.out.print("Enter the number for which you need to find the frequency: ");
+        int fNum= sc.nextInt();
+
+        int count = 0;
+        while(num>0){
+            int rem = num%10;
+            if (rem==fNum){
+                count++;
+            }
+            num = num/10;
+        }
+        System.out.println("Frequency =" +count);
+    }
+}
