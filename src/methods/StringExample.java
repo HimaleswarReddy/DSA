a41b2b0129164da7fa0774dab60a109e88b25399
Himaleswar Reddy <120051052+HimaleswarReddy@users.noreply.github.com>
2024-04-04 20:00:32 +0530

    returning string.

---
+++ src/methods/StringExample.java
@@ -0,0 +1,11 @@
+package methods;
+
+public class StringExample {
+    public static void main(String[] args) {
+        String message = greet();
+        System.out.println(message);
+    }
+    static String greet(){
+        return "How are you...?";
+    }
+}
