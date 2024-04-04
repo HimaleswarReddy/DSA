44d036ceb1592125ed72f3056d7827b08f025eb4
Himaleswar Reddy <120051052+HimaleswarReddy@users.noreply.github.com>
2024-04-04 20:00:51 +0530

    data change

--- src/methods/Sum.java
+++ src/methods/Sum.java
@@ -4,10 +4,18 @@ import java.util.Scanner;
 
 public class Sum {
     public static void main(String[] args) {
+        int ans = sum2();
+        System.out.println(ans);
         sum();
-        sum();
-
-
+    }
+    static int sum2(){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter first number:");
+        int num1 = sc.nextInt();
+        System.out.print("Enter second number:");
+        int num2 = sc.nextInt();
+//        int sum = num1+num2;
+        return num2+num1;
     }
     static void sum(){
         Scanner sc = new Scanner(System.in);
