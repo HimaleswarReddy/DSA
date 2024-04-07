6bcfb12cbfe529f635241c86f3d71abcbecf034c
Himaleswar Reddy <120051052+HimaleswarReddy@users.noreply.github.com>
2024-04-07 23:40:20 +0530

    change.

--- src/KunalClasses/methods/Greeting.java
+++ src/KunalClasses/methods/Greeting.java
@@ -1,4 +1,4 @@
-package methods;
+package KunalClasses.methods;
 
 public class Greeting {
     public static void main(String[] args) {
--- src/KunalClasses/methods/Sum.java
+++ src/KunalClasses/methods/Sum.java
@@ -1,4 +1,4 @@
-package methods;
+package KunalClasses.methods;
 
 import java.util.Scanner;
 
