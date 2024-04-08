8b664e08a70c737f4190f626f93b2d7a5b9fa4e9
Himaleswar Reddy <120051052+HimaleswarReddy@users.noreply.github.com>
2024-04-08 14:59:48 +0530

    variable number of arguments.

---
+++ src/KunalClasses/methods/VarArgs.java
@@ -0,0 +1,13 @@
+package KunalClasses.methods;
+
+import java.util.Arrays;
+
+public class VarArgs {
+    public static void main(String[] args) {
+        fun();
+
+    }
+    static void fun(int ...v){
+        System.out.println(Arrays.toString(v));
+    }
+}
