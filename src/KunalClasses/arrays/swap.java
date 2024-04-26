9b8bfc2760aaafef9077c90e05bf607871eff126
Himaleswar Reddy <120051052+HimaleswarReddy@users.noreply.github.com>
2024-04-27 03:51:28 +0530

    reversing array

--- src/KunalClasses/arrays/swap.java
+++ src/KunalClasses/arrays/swap.java
@@ -5,8 +5,18 @@ import java.util.Arrays;
 public class swap {
     public static void main(String[] args) {
         int [] arr={1, 2, 3, 4, 5, 6};
-        swap(arr,2,3);
+        //swap(arr,2,3);
+        reverse(arr);
         System.out.println(Arrays.toString(arr));
+    }
+    static void reverse(int[] arr){
+        int start = 0;
+        int end = arr.length-1;
+        while(start<end){
+            swap(arr,start,end);
+            start++;
+            end--;
+        }
     }
     static void swap(int [] arr,int index1,int index2){
         int temp = arr[index1];
