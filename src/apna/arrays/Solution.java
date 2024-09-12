1539a738ccb132edac35af0b503e0c2218dc2a72
himaleswarreddy <himaleswarreddy@gmail.com>
2024-09-12 05:58:18 +0530

    Arrays sum of largest two to get maxsum

---
+++ src/apna/arrays/Solution.java
@@ -0,0 +1,26 @@
+package apna.arrays;
+import java.util.*;
+
+class Solution {
+    public static void main(String[] args) {
+        List<Integer> arr = new ArrayList<>(Arrays.asList(228, 394, 463, 227, 388, 757, 782 ,238, 967));
+        System.out.println(pairWithMaxSum(arr));
+    }
+    // Function to find pair with maximum sum
+    public static int pairWithMaxSum(List<Integer> arr) {
+        // Your code goes here
+        int largest = 1;
+        int sl = 1;
+
+        for (int i = 0; i < arr.size(); i++) {
+            if(arr.get(i) > largest) {
+                sl = largest;
+                largest = arr.get(i);
+            } else if (arr.get(i) < largest && arr.get(i) > sl) {
+                sl = arr.get(i);
+            }
+        }
+
+        return largest + sl;
+    }
+}
