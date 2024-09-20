95904988c0bf5c8bb494dc1bb8e43f7795f7350c
himaleswarreddy <himaleswarreddy@gmail.com>
2024-09-20 22:40:18 +0530

    queue using Array

---
+++ src/apna/queue/CircularQueueUsingArray.java
@@ -0,0 +1,14 @@
+package apna.queue;
+
+
+
+public class CircularQueueUsingArray {
+
+    static class Queue {
+        int[] arr;
+        int size;
+        int rear;
+        int front;
+
+    }
+}
