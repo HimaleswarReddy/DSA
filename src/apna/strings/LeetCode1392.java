3de8cffd999a1ee77274b348a89197eae2935fe3
himaleswarreddy <himaleswarreddy@gmail.com>
2024-09-26 23:02:48 +0530

    bruteforce but TLE

---
+++ src/apna/strings/LeetCode1392.java
@@ -0,0 +1,20 @@
+package apna.strings;
+
+
+// BruteForce approach
+public class LeetCode1392 {
+    public static void main(String[] args) {
+        System.out.println(longestPrefix("level"));
+    }
+    public static String longestPrefix(String s) {
+
+        String str = "";
+
+        for(int i = 0; i < s.length(); i++) {
+            if (s.substring(0, i).equals(s.substring(s.length()  - i))) {
+                str = s.substring(0, i);
+            }
+        }
+        return str;
+    }
+}
